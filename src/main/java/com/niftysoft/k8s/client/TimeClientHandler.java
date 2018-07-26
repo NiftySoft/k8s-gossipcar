a32ae15d06b5bee2bfb6065537400b8d8e944aeb
K. Alex Mills <k.alex.mills@gmail.com>
2018-07-26 14:27:57 -0500

    Added netty examples.

---
+++ src/main/java/com/niftysoft/k8s/client/TimeClientHandler.java
@@ -0,0 +1,29 @@
+package com.niftysoft.k8s.client;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.util.Date;
+
+public class TimeClientHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf m = (ByteBuf) msg;
+
+        try {
+            long currentTimeMillis = (m.readUnsignedInt() - 220898800L) * 1000L;
+            System.out.println(new Date(currentTimeMillis));
+            ctx.close();
+        } finally {
+            m.release();
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
---
+++ src/main/java/com/niftysoft/k8s/server/GossipServer.java
@@ -0,0 +1,55 @@
+package com.niftysoft.k8s.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class GossipServer {
+    private int port;
+
+    public GossipServer(int port) {
+        this.port = port;
+    }
+
+    public void run() throws Exception {
+        // TODO: Allow the number of boss and worker threads to be configurable.
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+             .channel(NioServerSocketChannel.class)
+             .childHandler(new ChannelInitializer<SocketChannel>(){
+                @Override
+                 public void initChannel(SocketChannel ch) throws Exception {
+                    ch.pipeline().addLast(new GossipServerHandler());
+                }
+             })
+             .option(ChannelOption.SO_BACKLOG, 128)
+             .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            ChannelFuture f = b.bind(port).sync();
+
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port;
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        } else {
+            port = 46747;
+        }
+
+        new GossipServer(port).run();
+    }
+}
