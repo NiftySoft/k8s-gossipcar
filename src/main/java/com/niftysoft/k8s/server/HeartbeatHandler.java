92f5ed6802bbed477f8c26f08d59af1cbe35ea6f
K. Alex Mills <k.alex.mills@gmail.com>
2018-07-27 18:54:31 -0500

    Test HeartBeatHandler

--- src/main/java/com/niftysoft/k8s/server/HeartbeatHandler.java
+++ src/main/java/com/niftysoft/k8s/server/HeartbeatHandler.java
@@ -3,13 +3,18 @@ package com.niftysoft.k8s.server;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
+import java.nio.charset.Charset;
+
 public class HeartbeatHandler extends ChannelInboundHandlerAdapter {
-    public void channelRead(ChannelHandlerContext ctx, Object msg) {
-        // This five-byte string has the advantage of being the same in big-endian and little-endian encodings.
-        ctx.write("bb bb".getBytes());
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        // This five-byte string has the advantage of being the same in both big-endian and little-endian encodings.
+        // It is also really cute.
+        ctx.write(ctx.alloc().buffer(5).writeBytes("bb bb".getBytes(Charset.forName("UTF-8"))));
         ctx.flush();
     }
 
+    @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
         cause.printStackTrace();
         ctx.close();
