4d6428418cba04bc32f644049e5e72ad2d2551c9
K. Alex Mills <k.alex.mills@gmail.com>
2018-07-28 14:19:27 -0500

    Unused import

--- src/test/java/com/niftysoft/k8s/client/SyncClientHandlerTest.java
+++ src/test/java/com/niftysoft/k8s/client/SyncClientHandlerTest.java
@@ -16,7 +16,6 @@ import java.io.PrintStream;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
