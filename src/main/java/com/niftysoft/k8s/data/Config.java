5793eb1601a25f1f0b48310319e307edf92d3724
K. Alex Mills <k.alex.mills@gmail.com>
2018-07-27 20:06:17 -0500

    Remove GossipServerTest, enable SyncTask to avoid syncing with same pod.

--- src/main/java/com/niftysoft/k8s/data/Config.java
+++ src/main/java/com/niftysoft/k8s/data/Config.java
@@ -1,13 +1,20 @@
 package com.niftysoft.k8s.data;
 
 public final class Config {
-    private static final String PEER_PORT = "PEER_PORT";
-    private static final String CLIENT_PORT = "CLIENT_PORT";
-    private static final String SERVICE_DNS_NAME = "SERVICE_DNS_NAME";
+    public static final String PEER_PORT = "PEER_PORT";
+    public static final String CLIENT_PORT = "CLIENT_PORT";
+    public static final String SERVICE_DNS_NAME = "SERVICE_DNS_NAME";
+    public static final String MY_POD_NAME = "MY_POD_NAME";
+    public static final String MY_POD_NAMESPACE = "MY_POD_NAMESPACE";
+    public static final String MY_POD_IP = "MY_POD_IP";
+
 
     public int peerPort = 46747;
     public int clientPort = 80;
     public String serviceDnsName = "gossipSidecar.default";
+    public String podIp = "127.0.0.1";
+    public String podNamespace = "default";
+    public String podName = "gossipSidecar-0";
 
     public static Config fromEnvVars() {
         Config result = new Config();
